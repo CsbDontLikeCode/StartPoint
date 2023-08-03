403ce6836a255ee046e6d204f8668b890d4196b9
CsbDontLikeCode <2698417371@qq.com>
2023-08-03 15:08:36 +0800

    Properties Panel

--- Editor/src/Panels/SceneHierachyPanel.cpp
+++ Editor/src/Panels/SceneHierachyPanel.cpp
@@ -1,9 +1,10 @@
 #include <imgui.h>
 #include <StartPoint/Scene/Components.h>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "SceneHierachyPanel.h"
 
-namespace StartPoint 
+namespace StartPoint
 {
 	SceneHierachyPanel::SceneHierachyPanel(const Ref<Scene>& context)
 	{
@@ -28,6 +29,9 @@ namespace StartPoint
 			//ImGui::Text("%s", tag.Tag.c_str());
 		}
 
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+			m_SelectionContext = {};
+
 		// TODELETE::The following code is invalid.
 		//m_Context->m_Registry.each([&](auto entityID)
 		//{
@@ -36,6 +40,12 @@ namespace StartPoint
 		//	ImGui::Text("%s", tc.Tag);
 		//});
 
+		ImGui::End();
+
+		// Properties edit.
+		ImGui::Begin("Properties");
+		if (m_SelectionContext)
+			DrawComponents(m_SelectionContext);
 		ImGui::End();
 	}
 	void SceneHierachyPanel::DrawEntityNode(Entity entity)
@@ -54,6 +64,34 @@ namespace StartPoint
 		{
 			ImGui::TreePop();
 			ImGui::Text("%s", tag.c_str());
+		}
+	}
+
+	void SceneHierachyPanel::DrawComponents(Entity entity)
+	{
+		// TagComponent.
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		// TransformComponent.
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+				ImGui::TreePop();
+			}
 		}
 	}
 }
